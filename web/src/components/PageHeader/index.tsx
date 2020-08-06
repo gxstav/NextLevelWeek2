94690a2c4c51f18043d5deea0d3215eb16ec9ccb
gxstav <gustavoscbarcelos@gmail.com>
2020-08-05 22:36:19 -0300

    Day 3

--- web/src/components/PageHeader/index.tsx
+++ web/src/components/PageHeader/index.tsx
@@ -4,28 +4,31 @@ import { Link } from 'react-router-dom';
 import logoImg from '../../assets/images/logo.svg';
 import backIcon from '../../assets/images/icons/back.svg';
 
-import './style.css'
+import './style.css';
 
 interface PageHeaderProps {
   title: string;
+  description?: string;
 }
 
 const PageHeader: React.FC<PageHeaderProps> = (props) => {
   return (
     <header className="page-header">
-        <div className="top-bar-container">
-          <Link to="/">
-            <img src={backIcon} alt="Voltar"/>
-          </Link>
-          <img src={logoImg} alt="Proffy"/>
-        </div>
+      <div className="top-bar-container">
+        <Link to="/">
+          <img src={backIcon} alt="Voltar"/>
+        </Link>
+        <img src={logoImg} alt="Proffy"/>
+      </div>
 
-        <div className="header-content">
-          <strong>{props.title}</strong>
+      <div className="header-content">
+        <strong>{props.title}</strong>
 
-          {props.children}
-        </div>
-      </header>
+        {props.description && <p>{props.description}</p>}
+
+        {props.children}
+      </div>
+    </header>
   );
 }
 
